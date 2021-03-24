e79702e1a546454610492b82be016b40bce68d65
Emanuele Correa <manuncorrea@hotmail.com>
2021-03-24 00:22:27 -0300

    static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -33,7 +33,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IYKtGINrSo1HhxOYqGeWV7f')
 
   const product = {
@@ -46,6 +46,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return{
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
   }
 }
