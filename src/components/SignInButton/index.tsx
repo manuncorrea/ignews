bce9bdeef3cd81e29bc717b748cd5c7d057e21de
Emanuele Correa <manuncorrea@hotmail.com>
2021-03-25 13:08:33 -0300

    configurando o faunadb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,6 +1,6 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
-import { signIn, useSession } from 'next-auth/client';
+import { signIn, signOut,  useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
@@ -11,9 +11,13 @@ export function SignInButton() {
 
   return session ? (
     // se o o usuario estiver logado exibir esse button:
-    <button className={styles.signInButton} type="button">
+    <button 
+      className={styles.signInButton} 
+      type="button" 
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-        Emanuele Correa
+        {session.user.name}
       <FiX color="#737388" className={styles.closeIcon}/>
     </button>
   ) : (
